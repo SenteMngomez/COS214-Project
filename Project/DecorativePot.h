f3017f55b48c465e413d64d052c7ee48f4945204
Brayden-ux <brayden9011@gmail.com>
2025-10-27 01:39:14 +0200

    Decorator implementation

--- Project/DecorativePot.h
+++ Project/DecorativePot.h
@@ -1,13 +1,40 @@
+/**
+ * @file DecorativePot.h
+ * @brief Decorator for adding a decorative pot to plants
+ * @author Brayden
+ * @date October 26, 2025
+ */
+
 #ifndef DECORATIVEPOT_H
 #define DECORATIVEPOT_H
 
 #include "PlantDecorator.h"
 
+/**
+ * @class DecorativePot
+ * @brief Decorator that adds a decorative pot to a plant
+ * 
+ * Enhances a plant with a pot for sales/customization.
+ */
 class DecorativePot :public PlantDecorator {
-
+private:
+	string potType;
 
 public:
-	void print();
+	/**
+     * @brief Construct a DecorativePot decorator
+     * @param plant Pointer to the plant
+     * @param pot The pot type
+     * @param colour Plant colour
+     * @param careStrategy Care strategy
+     * @param price Plant price
+     */
+    DecorativePot(Plant* plant, string pot, string colour, CareStrategy* careStrategy, double price);
+
+	/**
+     * @brief Print the plant with pot details
+     */
+	void print() override;
 };
 
 #endif
--- Project/PlantDecorator.h
+++ Project/PlantDecorator.h
@@ -1,14 +1,41 @@
+/**
+ * @file PlantDecorator.h
+ * @brief Base decorator for plant customizations
+ * @author Brayden
+ * @date October 26, 2025
+ */
+
 #ifndef PLANTDECORATOR_H
 #define PLANTDECORATOR_H
 
 #include "Plant.h"
+#include <iostream>
 
+/**
+ * @class PlantDecorator
+ * @brief Base class for decorating plants with additional features
+ * 
+ * PlantDecorator wraps a Plant to add customizations like pots or wrapping
+ * without modifying the core Plant class.
+ */
 class PlantDecorator :public Plant {
-
-public:
+private:
 	Plant* component;
 
-	void print();
+public:
+	/**
+     * @brief Construct a decorator around a plant
+     * @param plant Pointer to the plant to decorate
+     * @param colour Inherited color
+     * @param careStrategy Inherited care strategy
+     * @param price Inherited price
+     */
+    PlantDecorator(Plant* plant, string colour, CareStrategy* careStrategy, double price);	
+
+	/**
+     * @brief Print the decorated plant
+     */
+	void print() override;
 };
 
 #endif
